57bb5a303f1ead044983473e391942cda95c220c
zhenrong1999 <zhenrong1999@student.usm.my>
2019-03-05 21:00:35 +0800

    Add more feature
    Feature added:
    -sorting according to name and no. matric.
    -filtering gender and first alphabet of the name.
    -menu added for filter and keyin
    -key in info manually

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <cctype>
 #include <string>
 #include <sstream>
+#include <algorithm>
 using namespace std;
 
 class profile
@@ -16,12 +17,12 @@ class profile
     public:
     profile()
     {
-        name="null";
+        name="empty";
         nomatric=0;
-        gender="null";
-        address="null";
-        email="null";
-        nophone="null";
+        gender="empty";
+        address="empty";
+        email="empty";
+        nophone="empty";
     }
 
     profile(string Name, int Nomatric, string Gender, string Address, string Email, string NoPhone)
@@ -75,75 +76,130 @@ bool emailcheck(const string X);
 void readline(string line,profile student[],int i);
 int main()
 {
-    ifstream readf("file1.txt");
+    int j;int i=0;
+    profile student[100];
+    bool cond=true;
+    while(cond)
+    {
+    system("cls");
+    cout<<"Input data by"<<endl<<"1. Key in\n2. Files\n3. Cond."<<endl;
+    cin>>j;
+    if(j==1) //key in for the student info manually
+    {
+    string Name,a,gender,address,email,nophone;
+    int nomatric;
+    system("cls");
+    cout<<"Name of the student: "<<endl;
+    cin.clear();cin.ignore();
+    getline(cin,Name);
+    cout<<"No. matric of the student: "<<endl;
+    cin>>nomatric;
+    cout<<"Gender of the student(male/female): "<<endl;
+    cin>>gender;
+    cout<<"Address of the student: "<<endl;
+    cin.clear();cin.ignore();
+    getline(cin,address);
+    cout<<"Email of the student: "<<endl;
+    cin>>email;
+    cout<<"Phone number of the student: "<<endl;
+        cin>>nophone;
+        cout<<Name<<" " <<nomatric<<" " <<gender<<" "<<address<<" "<<email<< " "<<nophone<<endl;
+        student[i].change(Name,nomatric,gender,address, email,nophone);
+        cout<<student[i].getname()<<" "<<student[i].getmatric()<<" "<<student[i].getgender()<<" "<<student[i].getaddress()<<" "<<student[i].getemail()<<" "<<student[i].getnophone(); // put into the class as a member
+
+        i++;
+    }
+    else if(j==2)
+{
+    ifstream readf("file1.txt"); //read file function
     if (!readf)
         cout<<"Fail to read file";
     else
         {
                 string line;
-                int i=0;
-                profile student[100];
-                while(!readf.eof())
+
+                while(!readf.eof()) //start reading
                 {
                 getline(readf,line);
                 readline(line,student,i);
                 i++;
             }
-                cout<<" sucess..."<<endl;
+                cout<<" sucess..."<<endl; //read finished
+        }
+}
+    else if(j==3)   // exit the loop for the menu
+        cond=false;
+}
                 system("cls");
-                cout<<"Filter \n";
-                cout<<"1. Gender \n2. Display All \n3. Name\n4. \n";
-                int j;
+                cout<<"Filter \n";      //filter menu
+                cout<<"1. Gender \n2. Display All \n3. Name\n4. Sorting\n";
+
                 cin>>j;
-                if(j==1)
-                {
+                switch (j)
+                    {
+                case 1:
                     cout<< "1. Filter as Male \n"<<"2. Filter as Female \n";
                     cin>>j;
                     if(j==1)
                         for(int k=0;k<i;k++)
-                            if(student[k].getgender()=="male")
+                            if(student[k].getgender()=="male")  //search one by one for male
                                 cout<<student[k].getname()<<" "<<student[k].getmatric()<<" "<<student[k].getgender()<<" "<<student[k].getaddress()<<" "<<student[k].getemail()<<" "<<student[k].getnophone()<<endl;
                         else if(j==2)
                             for(int k=0;k<i;k++)
-                                if(student[k].getgender()=="female")
+                                if(student[k].getgender()=="female")     //search one by one for female
                                     cout<<student[k].getname()<<" "<<student[k].getmatric()<<" "<<student[k].getgender()<<" "<<student[k].getaddress()<<" "<<student[k].getemail()<<" "<<student[k].getnophone()<<endl;
-                }
-                if(j==2)
-                    for(int k=0;k<i;k++)
+                    break;
+                case 2:
+                    for(int k=0;k<i;k++)     //show all
                         cout<<student[k].getname()<<" "<<student[k].getmatric()<<" "<<student[k].getgender()<<" "<<student[k].getaddress()<<" "<<student[k].getemail()<<" "<<student[k].getnophone()<<endl;
-                if(j==3)
-                {
+                    break;
+                case 3:
                     char str;
-                    char a;
                     cout<<"First Alphabet : \n";
                     cin>>str;
                     for(int k=0;k<i;k++)
                     {
-                        a=student[k].getname()[0];
-                        if(a==str)
+                        if(student[k].getname().at(0)==str)  //search one by one for the first alphabet
                             cout<<student[k].getname()<<" "<<student[k].getmatric()<<" "<<student[k].getgender()<<" "<<student[k].getaddress()<<" "<<student[k].getemail()<<" "<<student[k].getnophone()<<endl;
                     }
+                    break;
+                case 4:
+                    cout<<"1. By Name\n2.By No. Matric\n";
+                    cin>>j;
+                    if (j==1)
+                    {
+                        sort(student, student + i,[](profile  a, profile  b) -> bool{ return a.getname() < b.getname(); } );    //sorting the student according to the name of the student
+                        for(int k=0;k<i;k++)
+                        cout<<student[k].getname()<<" "<<student[k].getmatric()<<" "<<student[k].getgender()<<" "<<student[k].getaddress()<<" "<<student[k].getemail()<<" "<<student[k].getnophone()<<endl;
+                    }
+                    else if(j==2)
+                    {
+                        sort(student, student + i,[](profile  a, profile  b) -> bool{ return a.getmatric() < b.getmatric(); } );    //sorting according to the no. matric
+                        for(int k=0;k<i;k++)
+                        cout<<student[k].getname()<<" "<<student[k].getmatric()<<" "<<student[k].getgender()<<" "<<student[k].getaddress()<<" "<<student[k].getemail()<<" "<<student[k].getnophone()<<endl;
+                    }
+                        break;
                 }
         }
-}
+
 
 void readline(string line,profile student[],int i)
 {
-    stringstream readl;
-    readl.str(line);
+    stringstream readl; //let the string like cout and cin
+    readl.str(line); //set line as the read
     string Name,a,gender,address,email,nophone;
     int nomatric;
     readl>>Name>>a;
     while(!checkfordigit(a))    //read text until numbers.
     {
-        Name.insert(Name.length()," "+a);
+        Name.insert(Name.length()," "+a); //add the string a after the original string name
         readl>>a;
     }
     nomatric=stoi(a);
     readl>>gender>>address>>email;
     while(!emailcheck(email))
     {
-        address.insert(address.length()," "+email);
+        address.insert(address.length()," "+email);  //add the string email after the original string address
         readl>>email;
     }
         readl>>nophone;
